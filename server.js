3b7f2541050c693a6c5e4147435f3e1bf2aeb0ac
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-04-22 18:26:59 -0300

    server created

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const ejs = require('ejs')
+const express =  require('express')
+const app = express()
+
+http.createServer({port:'3000'}, () => {
+    console.log('Server Opened')
+})
+
+app.set('view engine','ejs')
+app.set(express.static('./public/'))
+app.listen('3000',() => {
+    console.log('Listening in the port 3000')
+})
+
+// Middleware
+
+app.get('/', (req, res) => {
+    res.render('./index.ejs')
+})
+
+app.use((req, res) => {
+    res.status(404).render('./index.ejs')
+})
