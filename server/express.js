35e49e54c0984c106d5bab30ecc2b6a91b7870fe
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-04-29 15:21:57 -0300

    express moved from server.js to express.js

---
+++ server/express.js
@@ -0,0 +1,24 @@
+const ejs = require('ejs')
+const express =  require('express')
+const app = express()
+
+let server_data = {
+    server_port: 3000,
+    socket_port: 3009
+}
+
+app.set('view engine','ejs')
+app.use(express.static('./public'))
+app.listen(server_data.server_port,() => {
+    console.log('Listening in the port',server_data.server_port)
+})
+
+// Middleware
+
+app.get('/', (req, res) => {
+    res.render('./index.ejs',{data: {server_data}})
+})
+
+app.use((req, res) => {
+    res.status(404).render('./index.ejs',{data: {server_data}})
+})
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
-const ejs = require('ejs')
-const express =  require('express')
-const app = express()
 const ws = require('ws')
+
+require('./express.js')
 
 let server_data = {
     server_port: 3000,
@@ -108,20 +107,3 @@ function sendPlayers() {
     })
 }
 
-// Express
-
-app.set('view engine','ejs')
-app.use(express.static('./public'))
-app.listen(server_data.server_port,() => {
-    console.log('Listening in the port',server_data.server_port)
-})
-
-// Middleware
-
-app.get('/', (req, res) => {
-    res.render('./index.ejs',{data: {server_data}})
-})
-
-app.use((req, res) => {
-    res.status(404).render('./index.ejs',{data: {server_data}})
-})
