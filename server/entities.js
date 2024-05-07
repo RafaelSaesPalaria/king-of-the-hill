64d889d37e0d695cf5ccd58dba15913e16a5ad83
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-05-07 18:18:34 -0300

    is out of border detection

--- server/entities.js
+++ server/entities.js
@@ -48,13 +48,23 @@ module.exports = class Player {
         a.dy = -a.dy
     }
 
+    isOutOfBorders() {
+        return (
+            (this.x + this.r > 1366|| this.x + this.r < 0) ||
+            (this.y + this.r > 645  || this.y + this.r < 0))
+    }
+
     /**
      * @Called -----------------------------
      * @Do Change the position
      */
     update(ms) {
-
         this.y+=this.dy*3*ms
         this.x+=this.dx*3*ms
+
+        if (this.isOutOfBorders()) {
+            console.log(this+'is out of borders')
+        }
+
     }
 }
