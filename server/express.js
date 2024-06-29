60742aaa0b816a2b2470a291ad8fab063d6027c8
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-06-29 15:26:39 -0300

    server_config created

--- server/express.js
+++ server/express.js
@@ -1,11 +1,11 @@
 const ejs = require('ejs')
 const express =  require('express')
 const app = express()
+const fs = require('fs')
 
-module.exports = server_data = {
-    server_port: 3001,
-    socket_port: 3008
-}
+let server_data = fs.readFileSync('server/server_config.json','utf-8')
+server_data = JSON.parse(server_data)
+module.exports = server_data
 
 app.set('view engine','ejs')
 app.use(express.static('./public'))
