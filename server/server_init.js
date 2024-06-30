97262b7136f49fdf1e797a33cf9da9613549015f
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-06-30 11:27:31 -0300

    serverdata websocket dependency inversion

--- server/server_init.js
+++ server/server_init.js
@@ -1,2 +1,9 @@
-require('./web_socket')
+const fs = require('fs')
+let server_data = fs.readFileSync('server/server_config.json','utf-8')
+server_data = JSON.parse(server_data)
+
+let ws = require('./web_socket')
+ws.setData(server_data)
+ws.load()
+
 require('./express')
--- server/web_socket.js
+++ server/web_socket.js
@@ -1,106 +1,112 @@
 const ws = require('ws')
-const fs = require('fs')
 
-let server_data = fs.readFileSync('server/server_config.json','utf-8')
-server_data = JSON.parse(server_data)
+let server_data = undefined
 
-let Player = require('../public/assets/entities.js')
+function setData(data) {
+    server_data = data
+}
 
-// WebSocket
+function load() {
 
-let connections = []
-let players = []
+    let Player = require('../public/assets/entities.js')
 
-let wss = new ws.Server(
-    {port:server_data.socket_port})
+    // WebSocket
 
-let lastUpdate = Date.now()
+    let connections = []
+    let players = []
+
+    let wss = new ws.Server(
+        {port:server_data.socket_port})
+
+    let lastUpdate = Date.now()
 
 
-wss.on('connection', (stream) => {
-    console.log('Someone has entered')
-    
-    
-    // Create Player
-    let player = (new Player(
-        Math.floor(50+(Math.random()*(1366-100))),
-        Math.floor(50+(Math.random()*(645-100))),
-        50))
+    wss.on('connection', (stream) => {
+        console.log('Someone has entered')
+        
+        
+        // Create Player
+        let player = (new Player(
+            Math.floor(50+(Math.random()*(1366-100))),
+            Math.floor(50+(Math.random()*(645-100))),
+            50))
 
-    // Join Player and Stream
-    let con = {
-        "stream":stream,
-        "player":player}
+        // Join Player and Stream
+        let con = {
+            "stream":stream,
+            "player":player}
 
-    /**
-     * @Called When a connection is established
-     * @Do set a listener to the stream to update the player moviment and update the other streams
-     */
-    stream.on('message', (message) => {
-        let body = JSON.parse(message.toString())
-        if (body["todo"]==="key-update") {
-            con.player.changeDir(body["dx-axis"],body["dy-axis"])
-        }
+        /**
+         * @Called When a connection is established
+         * @Do set a listener to the stream to update the player moviment and update the other streams
+         */
+        stream.on('message', (message) => {
+            let body = JSON.parse(message.toString())
+            if (body["todo"]==="key-update") {
+                con.player.changeDir(body["dx-axis"],body["dy-axis"])
+            }
 
-        updatePlayers()
-        sendPlayers()
-        lastUpdate = Date.now()
-    })
+            updatePlayers()
+            sendPlayers()
+            lastUpdate = Date.now()
+        })
 
-    /**
-     * @Called When a connection is established
-     * @Do set a listener in the stream to remove the stream when closed
-     */
-    con.stream.on('close', () => {
-        connections = connections.filter(item => item.stream!==con.stream)
+        /**
+         * @Called When a connection is established
+         * @Do set a listener in the stream to remove the stream when closed
+         */
+        con.stream.on('close', () => {
+            connections = connections.filter(item => item.stream!==con.stream)
+            players = []
+            connections.forEach(con => {
+                players.push(con.player)
+            })
+        })
+
+        // Save Stream
+        connections.push(con)
+
         players = []
         connections.forEach(con => {
             players.push(con.player)
         })
+
     })
 
-    // Save Stream
-    connections.push(con)
+    function updatePlayers() {
+        let ms = Date.now() - lastUpdate
 
-    players = []
-    connections.forEach(con => {
-        players.push(con.player)
-    })
+        let playersC = Object.assign([],players)
 
-})
-
-function updatePlayers() {
-    let ms = Date.now() - lastUpdate
-
-    let playersC = Object.assign([],players)
-
-    playersC.forEach(pi => {
-        playersC.forEach(pe => {
-            if (pi!==pe) {
-                if (pi.checkCollision(pe)) {
-                    pi.collide(pe)
+        playersC.forEach(pi => {
+            playersC.forEach(pe => {
+                if (pi!==pe) {
+                    if (pi.checkCollision(pe)) {
+                        pi.collide(pe)
+                    }
                 }
-            }
+            })
+            playersC.shift()
         })
-        playersC.shift()
-    })
 
-    connections.forEach(con => {
-        con.player.update(ms/10)
-    })
+        connections.forEach(con => {
+            con.player.update(ms/10)
+        })
+    }
+
+    /**
+     * @Called When a player send a message to the server (player changeDirs)
+     * @Do Send the all players x,y,xd,yd to all connections 
+     */
+    function sendPlayers() {
+        connections.forEach(con => {
+            con.stream.send(JSON.stringify({
+                "todo":"render-players",
+                "timestamp":Date.now(),
+                "me":con.player,
+                "players":players.filter(player => player!==con.player)}))
+        })
+    }
 }
 
-/**
- * @Called When a player send a message to the server (player changeDirs)
- * @Do Send the all players x,y,xd,yd to all connections 
- */
-function sendPlayers() {
-    connections.forEach(con => {
-        con.stream.send(JSON.stringify({
-            "todo":"render-players",
-            "timestamp":Date.now(),
-            "me":con.player,
-            "players":players.filter(player => player!==con.player)}))
-    })
-}
-
+module.exports = {load, setData}
