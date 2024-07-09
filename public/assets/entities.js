a6ad135bc4d7902d7a479bdf7e929b8dfd18340a
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-07-09 10:49:09 -0300

    distance server documented

--- public/assets/entities.js
+++ public/assets/entities.js
@@ -21,6 +21,15 @@ module.exports = class Player {
         this.dy+= dy*this.f
     }
 
+    /**
+    * @Called by the check collision of the server
+    * @Do Return the 2d distance between the players center
+    * @param {Number} x position on the x-axis of the players 1
+    * @param {Number} y position on the y-axis of the players 1
+    * @param {Number} x2 position on the x-axis of the players 2
+    * @param {Number} y2 position on the y-axis of the players 2
+     * @returns the 2d distance between the players center
+    */
     distance(x,y,x2,y2) {
         let xDist = x2-x
         let yDist = y2-y
